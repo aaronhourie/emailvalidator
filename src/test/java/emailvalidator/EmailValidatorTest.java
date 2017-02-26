77b40e7d8eaa6e06f6a3e9982bed2dab90184d80
aaron <aaronhourie@gmail.com>
2017-02-26 12:52:21 -0400

    Added more test cases to the accepted email test to improve coverage

--- src/test/java/emailvalidator/EmailValidatorTest.java
+++ src/test/java/emailvalidator/EmailValidatorTest.java
@@ -6,12 +6,23 @@ import org.junit.Test;
 
 public class EmailValidatorTest {
 
+  
+  private final static String[] acceptedEmails = 
+    {"john@email.com",
+     "jane@email.com",
+     "bob.smith@email.com",
+     "alice@email.co.uk",
+    };
 
   @Test
   public void testAcceptedEmails() {
    EmailValidator emailValidator = EmailValidator.getInstance();
    assertNotNull(emailValidator);
-   boolean result = emailValidator.isValid("john@email.com");
-   assertTrue(result);
+   
+   
+   for (String email : acceptedEmails){
+     boolean result = emailValidator.isValid(email);
+     assertTrue(result);   
+   }
   }
 }
