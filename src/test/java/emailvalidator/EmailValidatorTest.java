cfd5771f23ed82b820265233fa92821327a082e9
aaron <aaronhourie@gmail.com>
2017-02-26 12:58:27 -0400

    Added test case to test emails which are not acceptable

--- src/test/java/emailvalidator/EmailValidatorTest.java
+++ src/test/java/emailvalidator/EmailValidatorTest.java
@@ -13,16 +13,36 @@ public class EmailValidatorTest {
      "bob.smith@email.com",
      "alice@email.co.uk",
     };
-
+  
+  private final static String[] rejectedEmails = 
+    {"carolemailcom",
+     "johnemail.com",
+     "jane@emailcom",
+     "bob.smith@email@com",
+     "alice@email.co.uk",
+    };
+  
   @Test
   public void testAcceptedEmails() {
    EmailValidator emailValidator = EmailValidator.getInstance();
    assertNotNull(emailValidator);
    
-   
+   // Check all of the known acceptable email addresses
    for (String email : acceptedEmails){
      boolean result = emailValidator.isValid(email);
      assertTrue(result);   
    }
+  }
+  
+  @Test
+  public void TestRejectedEmails() {
+    EmailValidator emailValidator = EmailValidator.getInstance();
+    assertNotNull(emailValidator);
+
+    // Check all of the known unacceptable email addresses
+    for (String email : rejectedEmails){
+      boolean result = emailValidator.isValid(email);
+      assertFalse(result);   
+    } 
   }
 }
